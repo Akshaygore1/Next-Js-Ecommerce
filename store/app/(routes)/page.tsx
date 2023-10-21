5e132e0f9205f51dce49ae2f1d8c7dc1d1a8aa13
Akshaygore1 <akshaygore2301@gmail.com>
2023-10-21 20:38:59 +0530

    added store

---
+++ store/app/(routes)/page.tsx
@@ -0,0 +1,21 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard();
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        {billboard && <Billboard data={billboard} />}
+        {/* <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div> */}
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;

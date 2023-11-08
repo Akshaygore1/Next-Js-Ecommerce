bad58490d7b985afe74a3a0b31c40084003bad69
Akshaygore1 <akshaygore2301@gmail.com>
2023-11-08 15:18:38 +0530

    added navbar

--- admin-ui/components/Navbar.tsx
+++ admin-ui/components/Navbar.tsx
@@ -1,25 +1,25 @@
-import { UserButton, auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { UserButton, auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
-import StoreSwitcher from "@/components/store-switcher";
-import { MainNav } from "@/components/main-nav";
-import { ThemeToggle } from "@/components/theme-toggle";
-import prismadb from "@/lib/prismadb";
+import StoreSwitcher from "@/components/store-switcher"
+import { MainNav } from "@/components/main-nav"
+import { ThemeToggle } from "@/components/theme-toggle"
+import prismadb from "@/lib/prismadb"
 
 const Navbar = async () => {
-  const { userId } = auth();
+  const { userId } = auth()
 
   if (!userId) {
-    redirect('/sign-in');
+    redirect("/sign-in")
   }
 
   const stores = await prismadb.store.findMany({
     where: {
-      userId,
+      userId
     }
-  });
+  })
 
-  return ( 
+  return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
@@ -30,7 +30,7 @@ const Navbar = async () => {
         </div>
       </div>
     </div>
-  );
-};
- 
-export default Navbar;
+  )
+}
+
+export default Navbar
